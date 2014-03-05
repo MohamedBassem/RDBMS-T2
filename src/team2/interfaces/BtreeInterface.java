2c4c52d3b323749885b5df58685a0c9e3d9d255a
mohamed_bassem <bassem@delodi.net>
2014-03-05 22:50:42 +0200

    Adding Btree interface

---
+++ src/team2/interfaces/BtreeInterface.java
@@ -0,0 +1,25 @@
+package team2.interfaces;
+
+public interface BtreeInterface {
+	
+	public void insert(String key,String value);
+	
+	public String get(String key);
+	
+	public String getTableName();
+	
+	public String getColumnName();
+	
+	public boolean isPrimary();
+	
+	public void setTableName(String tableName);
+	
+	public void setColumnName(String columnName);
+	
+	public void setIsPrimary(boolean isPrimary);
+	
+	public void delete(String key);
+	
+	public String serialize();
+
+}
--- src/team2/util/btrees/BTree.java
+++ src/team2/util/btrees/BTree.java
@@ -1,5 +1,67 @@
 package team2.util.btrees;
 
-public class BTree {
+import team2.interfaces.BtreeInterface;
+
+public class BTree implements BtreeInterface{
+
+	@Override
+	public void insert(String key, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String get(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTableName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getColumnName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isPrimary() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setTableName(String tableName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setColumnName(String columnName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIsPrimary(boolean isPrimary) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(String key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String serialize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
