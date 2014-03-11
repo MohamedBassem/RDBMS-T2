e50cf9c6cad0a6091b3801551edfe7109b569061
mohamed_bassem <bassem@delodi.net>
2014-03-11 12:53:35 +0200

    Adding intial Properties file

---
+++ src/team2/util/Properties.java
@@ -0,0 +1,15 @@
+package team2.util;
+
+public class Properties {
+	CSVReader reader;
+	
+	public Properties(CSVReader reader){
+		this.reader = reader;
+		init();
+	}
+	
+	private void init(){
+		
+	}
+	
+}
