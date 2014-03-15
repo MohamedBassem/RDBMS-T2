4fe76c2d48f6e0741eee8035374c13c9b323d221
mohamed_bassem <bassem@delodi.net>
2014-03-15 21:44:42 +0200

    Fixing getlastpage method name and adding try - catch blocks

--- src/team2/commands/InsertCommand.java
+++ src/team2/commands/InsertCommand.java
@@ -1,8 +1,10 @@
 package team2.commands;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+import team2.exceptions.DBEngineException;
 import team2.interfaces.Command;
 import team2.util.CSVReader;
 import team2.util.Properties;
@@ -27,22 +29,34 @@ public InsertCommand(BTreeFactory btfactory, CSVReader reader, String tableName,
 }
 	@Override
 	public void execute() {
-		int lastPage = reader.lastPage(tableName);
+		int lastPage = reader.getLastPageIndex(tableName);
 		int lastRow = reader.getLastRow(tableName, lastPage);
 		
 		if(lastRow == properties.getMaximumPageSize()) {
 			lastPage++;
-			reader.createTablePage(tableName, lastPage);
-		}
+			try {
+				reader.createTablePage(tableName, lastPage);
+			} catch (DBEngineException e) {
+				e.printStackTrace();
+			}
+		}		
 		
-		int row = reader.appendToTable(tableName, lastPage, htblColNameValue);
-		ArrayList<String> indexedColumns = properties.getIndexedColumns(tableName);
-		
-		for (String column : indexedColumns) {
-			String pointer = tableName + " " + lastPage + " " + row;
-			BTree tree = btfactory.getBtree(tableName, column);
-			tree.insert(htblColNameValue.get(column), pointer, false);
+		try {
+			int row = reader.appendToTable(tableName, lastPage, htblColNameValue);
+			ArrayList<String> indexedColumns = properties.getIndexedColumns(tableName);
 			
+			for (String column : indexedColumns) {
+				String pointer = tableName + " " + lastPage + " " + row;
+				BTree tree;
+				try {
+					tree = btfactory.getBtree(tableName, column);
+					tree.insert(htblColNameValue.get(column), pointer, false);
+				} catch (DBEngineException | IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (DBEngineException e) {
+			e.printStackTrace();
 		}
 	}
 
