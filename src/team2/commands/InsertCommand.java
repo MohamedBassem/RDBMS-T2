7425599c3f8bdfbf1a7d4ac55ce8455f50769896
Alaa Maher <almgohar@almgohar-SATELLITE-L755.(none)>
2014-03-20 03:16:21 +0200

    Finalizing InsertCommand

--- src/team2/commands/InsertCommand.java
+++ src/team2/commands/InsertCommand.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Set;
 
+import javax.swing.text.TabableView;
+
+import sun.org.mozilla.javascript.xml.XMLLib.Factory;
 import team2.exceptions.DBEngineException;
 import team2.interfaces.Command;
 import team2.util.CSVReader;
@@ -13,69 +16,68 @@ import team2.util.btrees.BTreeAdopter;
 import team2.util.btrees.BTreeFactory;
 
 public class InsertCommand implements Command {
-	BTreeFactory btfactory;
+	BTreeFactory btFactory;
 	CSVReader reader;
 	String tableName;
 	Properties properties;
 	Hashtable<String,String> htblColNameValue;
 	
 	
-public InsertCommand(BTreeFactory btfactory, CSVReader reader, String tableName, 
+public InsertCommand(BTreeFactory btFactory, CSVReader reader, String tableName, 
 		Properties properties, Hashtable<String,String> htblColNameValue) {
-	this.btfactory = btfactory;
+	this.btFactory = btFactory;
 	this.reader = reader;
 	this.tableName = tableName;
 	this.properties = properties;
 	this.htblColNameValue = htblColNameValue;
 }
 	@Override
-	public void execute() throws DBEngineException {
+	public void execute() throws DBEngineException, IOException {
 		if(properties.getData().get(tableName) == null)
 			throw new DBEngineException("Table name is wrong or it doesn't exist.");
 		
-		int lastPage = reader.getLastPageIndex(tableName);
-		int lastRow = reader.getLastRow(tableName, lastPage);
-		
-		if(lastRow == properties.getMaximumPageSize()) {
-			lastPage++;
-			
-				reader.createTablePage(tableName, lastPage);		
-		}	
-		
+			BTreeAdopter tree;
 			Set<String> columns = htblColNameValue.keySet();
 			for (String column : columns) {
 				if(properties.getData().get(tableName).get(column) == null)
 					throw new DBEngineException("Column name is wrong or it doesn't exist.");
+				if(properties.isPrimaryKey(tableName, column)) {
+					if(htblColNameValue.get(column) == null)
+						throw new DBEngineException("The primary key can't be null.");
+					tree = btFactory.getBtree(tableName, column);
+							if(tree.find(htblColNameValue.get(column)) != null)
+								throw new DBEngineException("Th primary key you're trying to insert is not unique.");
+						
+					
+				}
+				if(properties.isForeignKey(tableName, column)) {
+					if(htblColNameValue.get(column) == null)
+						throw new DBEngineException("The primary key can't be null.");
+					String reference = properties.getReferenceColumn(tableName, column);
+					String[] references = reference.split(".");
+					BTreeAdopter refTree = btFactory.getBtree(references[0], references[1]);
+					if(refTree.find(htblColNameValue.get(column)) == null)
+						throw new DBEngineException("The foreign key you're trying to " +
+								"insert doesn't reference anything."); 	
+				}
 			}
 			
+			int lastPage = reader.getLastPageIndex(tableName);
+			int lastRow = reader.getLastRow(tableName, lastPage);
+			
+			if(lastRow == properties.getMaximumPageSize()) {
+				lastPage++;
+				
+					reader.createTablePage(tableName, lastPage);		
+			}	
 			int row = reader.appendToTable(tableName, lastPage, htblColNameValue);
 			ArrayList<String> indexedColumns = properties.getIndexedColumns(tableName);
 			
 			for (String column : indexedColumns) {
 				String pointer = tableName + " " + lastPage + " " + row;
-				BTreeAdopter tree;
-				
-					tree = btfactory.getBtree(tableName, column);
-					if(properties.isPrimaryKey(tableName, column)) {
-						if(htblColNameValue.get(column) == null)
-							throw new DBEngineException("The primary key can't be null.");
-						try {
-							if(tree.find(htblColNameValue.get(column)) != null)
-								throw new DBEngineException("Th primary key you're trying to insert is not unique.");
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-					
-					try {
-						tree.insert(htblColNameValue.get(column), pointer);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+					tree = btFactory.getBtree(tableName, column);
+					tree.insert(htblColNameValue.get(column), pointer);			
 			}
 		
-	}
 }
-
-
-
+}
--- src/team2/engine/Engine.java
+++ src/team2/engine/Engine.java
@@ -3,8 +3,10 @@ package team2.engine;
 import java.util.Hashtable;
 import java.util.Iterator;
 
+import team2.commands.CreateIndex;
 import team2.commands.CreateTableCommand;
 import team2.commands.DeleteCommand;
+import team2.commands.InsertCommand;
 import team2.commands.SelectCommand;
 import team2.exceptions.DBAppException;
 import team2.exceptions.DBEngineException;
@@ -37,14 +39,16 @@ public class Engine {
 	}
 
 	
-	public void createIndex(String strTableName, String strColName) throws DBAppException {
-		// TODO
+	public void createIndex(String strTableName, String strColName) throws DBAppException, DBEngineException {
+		CreateIndex newIndex = new CreateIndex(strTableName, strColName, properties, reader, bTreeFactory);
+		newIndex.execute();
 	}
 	
 	public void insertIntoTable(String strTableName,
 								Hashtable<String,String> htblColNameValue)
-										throws DBAppException {
-		// TODO
+										throws DBAppException, DBEngineException {
+		InsertCommand insert = new InsertCommand(bTreeFactory, reader, strTableName, properties, htblColNameValue);
+		insert.execute();
 	}
 	
 	public void deleteFromTable(String strTableName,
