9c43b349b2edcbec7d104cd70b8e5faafb7f8309
Rami Khalil <thesavagekoder@gmail.com>
2014-06-03 14:04:05 +0300

    Changed build path. Created empty class.

---
+++ src/eg/edu/guc/dbms/sqlparser/SQLParser.java
@@ -0,0 +1,13 @@
+package eg.edu.guc.dbms.sqlparser;
+
+public class SQLParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
