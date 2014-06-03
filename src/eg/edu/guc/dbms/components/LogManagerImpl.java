91f14ecc1060a8854cb968696d00721944492fd0
farghalico <mohamed@farghal.com>
2014-06-03 23:35:08 +0200

    add factory of log manager

--- src/eg/edu/guc/dbms/components/LogManagerImpl.java
+++ src/eg/edu/guc/dbms/components/LogManagerImpl.java
@@ -1,6 +1,57 @@
 package eg.edu.guc.dbms.components;
 
-public class LogManagerImpl {
+import java.util.Hashtable;
+
+import eg.edu.guc.dbms.interfaces.LogManager;
+
+public class LogManagerImpl implements LogManager{
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void flushLog() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recordStart(String strTransID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recordUpdate(String strTransID, String tableName,
+			int pageNummber, String strKeyValue, String strColName,
+			Object objOld, Object objNew) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recordInsert(String strTransID, String tableName,
+			int pageNumber, Hashtable<String, String> htblColValues) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recordDelete(String strTransID, String tableName,
+			int pageNumber, String strKeyValue,
+			Hashtable<String, String> htblColValues) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recordCommit(String strTransID) {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 }
