5df88329c497171aae37f44dbd0b5ad8fc5e44aa
MohamedBassem <medoox240@gmail.com>
2014-06-04 13:13:31 +0300

    Initial BufferManager Commit

--- src/eg/edu/guc/dbms/components/BufferManager.java
+++ src/eg/edu/guc/dbms/components/BufferManager.java
@@ -1,7 +1,37 @@
 package eg.edu.guc.dbms.components;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import eg.edu.guc.dbms.helpers.BufferQueueEntry;
+
 public class BufferManager {
-
-
+	
+	/**
+	 * The Queue of unused buffer slots
+	 */
+	Queue<BufferQueueEntry> unUsedSlots;
+	
+	/**
+	 * The hashMap containing the used slots 
+	 */
+	HashMap<Integer,BufferQueueEntry> usedSlots;
+	
+	
+	public void init(){
+		unUsedSlots = new LinkedList<BufferQueueEntry>();
+		usedSlots = new HashMap<Integer,BufferQueueEntry>();
+	}
+	
+	public synchronized void read(String tableName,int pageNumber){
+		
+	}
+	
+	// TODO The write phase
+	public synchronized void write(String tableName,int pageNumber){
+		
+	}
 
 }
---
+++ src/eg/edu/guc/dbms/helpers/BufferQueueEntry.java
@@ -0,0 +1,5 @@
+package eg.edu.guc.dbms.helpers;
+
+public class BufferQueueEntry {
+
+}
