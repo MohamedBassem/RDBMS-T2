4b47948bc94c09a92a0322a4f72329aee60bd08d
Alaa Maher <almgohar@gmail.com>
2014-06-04 16:08:09 +0200

    recovery

--- src/eg/edu/guc/dbms/components/RecoveryManager.java
+++ src/eg/edu/guc/dbms/components/RecoveryManager.java
@@ -1,6 +1,24 @@
 package eg.edu.guc.dbms.components;
 
-public class RecoveryManager {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Hashtable;
 
-	
+public class RecoveryManager {
+	FileReader logFile;
+	BufferedReader br;
+
+	public void recover() throws IOException {
+		logFile = new FileReader("data/log/logFile.log");
+		br = new BufferedReader(logFile);
+		Hashtable<String, Boolean> transactions = new Hashtable<String, Boolean>();
+		String line = br.readLine();
+		while (line != null) {
+			if (line.contains("<START ")) {
+				transactions.put(line.substring(7, line.length() - 1), false);
+			}
+		}
+
+	}
 }
