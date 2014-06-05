94a2a72e96d46ea993e9f9618962d4ad5af96107
sak93 <sarakhodeir@gmail.com>
2014-06-05 23:13:21 +0300

    add update

--- src/eg/edu/guc/dbms/commands/UpdateCommand.java
+++ src/eg/edu/guc/dbms/commands/UpdateCommand.java
@@ -1,30 +1,104 @@
 package eg.edu.guc.dbms.commands;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
+import eg.edu.guc.dbms.components.BufferManager;
 import eg.edu.guc.dbms.exceptions.DBEngineException;
+import eg.edu.guc.dbms.helpers.Page;
+import eg.edu.guc.dbms.helpers.Tuple;
 import eg.edu.guc.dbms.interfaces.Command;
 import eg.edu.guc.dbms.utils.CSVReader;
 import eg.edu.guc.dbms.utils.Properties;
+import eg.edu.guc.dbms.utils.Utils;
+import eg.edu.guc.dbms.utils.btrees.BTreeAdopter;
 import eg.edu.guc.dbms.utils.btrees.BTreeFactory;
 
 public class UpdateCommand implements Command {
-
-	public UpdateCommand(BTreeFactory btfactory,CSVReader reader,Properties properties,String tableName,
-			HashMap<String, String> htblColNameValue, String strOperator, HashMap<String, String> colValue) {
-		
+	CSVReader reader;
+	String tableName;
+	HashMap<String, String>hMapColNameValue;
+	HashMap<String, String> colValue;
+	String strOperator;
+	Properties properties;
+	BTreeFactory btfactory;
+	SelectCommand select;
+	BufferManager bufferManager;
+	ArrayList<String> rows;
+	int pageId;
+	int transactionNumber; 
+	public UpdateCommand(BTreeFactory btfactory, CSVReader reader,
+			Properties properties, String tableName,
+			HashMap<String, String>hMapColNameValue, ArrayList<String> rows,
+			String strOperator, HashMap<String, String> colValue,
+			BufferManager bufferManager, int transactionNumber) {
+		this.btfactory = btfactory;
+		this.reader = reader;
+		this.tableName = tableName;
+		this.hMapColNameValue =hMapColNameValue;
+		this.strOperator = strOperator;
+		this.colValue = colValue;
+		this.bufferManager = bufferManager;
+		this.rows = rows;
+		this.transactionNumber=transactionNumber; 
+		select = new SelectCommand(this.btfactory, this.reader,
+				this.properties, this.bufferManager, this.tableName,
+				this.hMapColNameValue, this.strOperator);
 	}
-	
-	
+
 	@Override
 	public void execute() throws DBEngineException, IOException {
-		// TODO Auto-generated method stub
+		select.execute();
+		this.validate(); 
+		this.updateTable();
+		this.updateTree();
 
 	}
 
+	public void updateTable() throws DBEngineException {
+		ArrayList<String> pointers = select.getResultPointers();
+		for (int i = 0; i < pointers.size(); i++) {
+			String[] x = ((String) pointers.get(i)).split(" ");
+			int pageNumber = Integer.parseInt(x[1]);
+			int rowNumber = Integer.parseInt(x[2]);
+			Page page = bufferManager.read(transactionNumber,tableName, pageNumber, true);
+			//HashMap<String, String> newHMap= updateValues(); 
+			//page.set(rowNumber, new Tuple(newHMap)); 
+			bufferManager.write(transactionNumber,tableName, pageNumber, page);
+		}
+	}
 
+	public void updateTree() throws DBEngineException, IOException {
+		ArrayList<String> indexedColumns = properties
+				.getIndexedColumns(tableName);
+		ArrayList<String> pointers = select.getResultPointers();
+		ArrayList<HashMap<String, String>> results = select.getResults();
+		for (int i = 0; i < indexedColumns.size(); i++) {
+			BTreeAdopter adoptor = btfactory.getBtree(tableName,
+					indexedColumns.get(i));
+			for (int j = 0; j < pointers.size(); j++) {
+				adoptor.delete(results.get(j).get(indexedColumns.get(i)),
+						pointers.get(j));
+				adoptor.insert(colValue.get(indexedColumns.get(i)),
+						pointers.get(j));
+			}
+		}
+	}
+	public HashMap<String,String> updateValues(HashMap<String,String> input){
+		Set<String> columnName =hMapColNameValue.keySet();
+		String [] columnNames = Utils.setToArray(columnName);   	
+		for(int i =0; i<columnNames.length; i++){
+			input.put(columnNames[i],hMapColNameValue.get(columnNames[i]));
+		}
+		return input; 
+	}
+	
+	public void validate(){
+		
+	}
 	@Override
 	public List<HashMap<String, String>> getResult() {
 		// TODO Auto-generated method stub
@@ -32,3 +106,4 @@ public class UpdateCommand implements Command {
 	}
 
 }
+
