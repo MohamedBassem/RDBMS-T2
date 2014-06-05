23433083e7ac19aa3c90ddfb408f0e5e327d874b
Rami Khalil <thesavagekoder@gmail.com>
2014-06-05 16:14:10 +0300

    Select/Update preparation.

--- src/eg/edu/guc/dbms/components/SQLParserImpl.java
+++ src/eg/edu/guc/dbms/components/SQLParserImpl.java
@@ -1,33 +1,58 @@
 package eg.edu.guc.dbms.components;
 
 import eg.edu.guc.dbms.interfaces.SQLParser;
+import eg.edu.guc.dbms.sql.PhysicalPlanTree;
+import gudusoft.gsqlparser.EDbVendor;
+import gudusoft.gsqlparser.TCustomSqlStatement;
+import gudusoft.gsqlparser.TGSqlParser;
+import gudusoft.gsqlparser.stmt.TSelectSqlStatement;
 
 public class SQLParserImpl implements SQLParser {
 	private static SQLParserImpl instance = new SQLParserImpl();
+	private TGSqlParser sqlparser;
+	private int status;
 	
 	private SQLParserImpl() {
-		
+		sqlparser = new TGSqlParser(EDbVendor.dbvoracle);
+		status = 0;
 	}
 	
 	public static SQLParserImpl getInstance() {
 		return instance;
 	}
 
-	@Override
 	public boolean parseSQLStatement(String statement) {
-		// TODO Auto-generated method stub
-		return false;
+		sqlparser.sqltext = statement; 
+		status = sqlparser.parse(); 
+		return status == 0;
 	}
 
-	@Override
 	public String getErrorMessage() {
-		// TODO Auto-generated method stub
-		return null;
+		if(status == 0)
+			return null;
+		return sqlparser.getErrormessage();
 	}
 
-	@Override
-	public Object getParseTree() {
-		// TODO Auto-generated method stub
-		return null;
+	public PhysicalPlanTree getParseTree() {
+		PhysicalPlanTree result = null;
+		if(status == 0) {
+			TCustomSqlStatement statement = sqlparser.sqlstatements.get(0);
+			switch (statement.sqlstatementtype) {
+			case sstselect:
+				// analyzeSelectStmt((TSelectSqlStatement)stmt);
+				break;
+			case sstupdate:
+				break;
+			case sstcreatetable:
+				break;
+			case sstaltertable:
+				break;
+			case sstcreateview:
+				break;
+			default:
+				System.out.println(statement.sqlstatementtype.toString());
+			}
+		}
+		return result;
 	}
 }
