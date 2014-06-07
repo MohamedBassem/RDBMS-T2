838c14dc85a4443642752264c0be59a0b2513ccf
sak93 <sarakhodeir@gmail.com>
2014-06-07 21:05:21 +0300

    bug fixes

--- src/eg/edu/guc/dbms/commands/UpdateCommand.java
+++ src/eg/edu/guc/dbms/commands/UpdateCommand.java
@@ -19,7 +19,7 @@ import eg.edu.guc.dbms.utils.btrees.BTreeFactory;
 public class UpdateCommand implements Command {
 	CSVReader reader;
 	String tableName;
-	HashMap<String, String>hMapColNameValue;
+	HashMap<String, String> hMapColNameValue;
 	HashMap<String, String> colValue;
 	String strOperator;
 	Properties properties;
@@ -27,33 +27,34 @@ public class UpdateCommand implements Command {
 	SelectCommand select;
 	BufferManager bufferManager;
 	int pageId;
-	long transactionNumber=1; 
+	long transactionNumber = 1;
+
 	public UpdateCommand(BTreeFactory btfactory, CSVReader reader,
 			Properties properties, String tableName,
-			HashMap<String, String>hMapColNameValue,
-			String strOperator, HashMap<String, String> colValue,
-			BufferManager bufferManager, long transactionNumber) {
+			HashMap<String, String> hMapColNameValue, String strOperator,
+			HashMap<String, String> colValue, BufferManager bufferManager,
+			long transactionNumber) {
 		this.btfactory = btfactory;
 		this.reader = reader;
 		this.tableName = tableName;
-		this.hMapColNameValue =hMapColNameValue;
+		this.hMapColNameValue = hMapColNameValue;
 		this.strOperator = strOperator;
 		this.colValue = colValue;
 		this.bufferManager = bufferManager;
-		this.properties=properties;
-		this.transactionNumber = transactionNumber; 
+		this.properties = properties;
+		this.transactionNumber = transactionNumber;
 		select = new SelectCommand(this.btfactory, this.reader,
 				this.properties, this.bufferManager, this.tableName,
 				this.colValue, this.strOperator, transactionNumber);
-	
+
 	}
 
 	@Override
 	public void execute() throws DBEngineException, IOException {
-		this.validate(); 
+		this.validate();
 		select.execute();
-		this.updateTable();
 		this.updateTree();
+		this.updateTable();
 
 	}
 
@@ -63,9 +64,10 @@ public class UpdateCommand implements Command {
 			String[] x = ((String) pointers.get(i)).split(" ");
 			int pageNumber = Integer.parseInt(x[1]);
 			int rowNumber = Integer.parseInt(x[2]);
-			Page page = bufferManager.read(transactionNumber,tableName, pageNumber, true);
+			Page page = bufferManager.read(transactionNumber, tableName,
+					pageNumber, true);
 			page.add(updateValues(page.get(rowNumber)));
-			bufferManager.write(transactionNumber,tableName, pageNumber, page);
+			bufferManager.write(transactionNumber, tableName, pageNumber, page);
 		}
 	}
 
@@ -78,35 +80,41 @@ public class UpdateCommand implements Command {
 			BTreeAdopter adoptor = btfactory.getBtree(tableName,
 					indexedColumns.get(i));
 			for (int j = 0; j < pointers.size(); j++) {
-				adoptor.delete(results.get(j).get(indexedColumns.get(i)),
-						pointers.get(j));
-		
-				adoptor.insert(indexedColumns.get(i),hMapColNameValue.get(indexedColumns.get(i)));
-				
+
+				if (hMapColNameValue.get(indexedColumns.get(i)) != null) {
+					adoptor.delete(results.get(j).get(indexedColumns.get(i)),
+							pointers.get(j));
+
+					adoptor.insert(indexedColumns.get(i),
+							hMapColNameValue.get(indexedColumns.get(i)));
+				}
 			}
 		}
 	}
-	public HashMap<String,String> updateValues(HashMap<String,String> input){
-		Set<String> columnName =hMapColNameValue.keySet();
-		String [] columnNames = Utils.setToArray(columnName);   	
-		for(int i =0; i<columnNames.length; i++){
-			input.put(columnNames[i],hMapColNameValue.get(columnNames[i]));	
+
+	public HashMap<String, String> updateValues(HashMap<String, String> input) {
+		Set<String> columnName = hMapColNameValue.keySet();
+		String[] columnNames = Utils.setToArray(columnName);
+		for (int i = 0; i < columnNames.length; i++) {
+			input.put(columnNames[i], hMapColNameValue.get(columnNames[i]));
 		}
-		return input; 
+		return input;
 	}
-	
-	public void validate() throws DBEngineException{
-		if(properties.getData().get(tableName)==null){
+
+	public void validate() throws DBEngineException {
+		if (properties.getData().get(tableName) == null) {
 			throw new DBEngineException("Table name is wrong or doesn't exist.");
 		}
-		Set<String> columnName =hMapColNameValue.keySet();
-		String [] columnNames = Utils.setToArray(columnName);   	
-		for(int i =0; i<columnNames.length; i++){
-			if(properties.getData().get(tableName).get(columnNames[i]) == null){
-				throw new DBEngineException("Column name is wrong or doesn't exist.");
+		Set<String> columnName = hMapColNameValue.keySet();
+		String[] columnNames = Utils.setToArray(columnName);
+		for (int i = 0; i < columnNames.length; i++) {
+			if (properties.getData().get(tableName).get(columnNames[i]) == null) {
+				throw new DBEngineException(
+						"Column name is wrong or doesn't exist.");
 			}
-		}		
+		}
 	}
+
 	@Override
 	public List<HashMap<String, String>> getResult() {
 		// TODO Auto-generated method stub
@@ -114,4 +122,3 @@ public class UpdateCommand implements Command {
 	}
 
 }
-
